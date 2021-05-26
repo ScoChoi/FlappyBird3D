745ec5ccabd044e13f05f3b4a95218a4d52c0d1c
Scott Choi <ascottychoi@gmail.com>
2021-05-25 21:10:02 -0700

    changes to count display

--- FB3D/Assets/Scripts/PlayerController.cs
+++ FB3D/Assets/Scripts/PlayerController.cs
@@ -118,7 +118,7 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-            countText.text = "Count: " + count.ToString();
+            countText.text = count.ToString();
             if (count > 66 && !changed)
             {
                 caller.ChangeMusic();
